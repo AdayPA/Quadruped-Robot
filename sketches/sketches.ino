eb86f7f28952f69bb490993b76e227d740ce19d8
dyto_aday <dyto_aday@hotmail.com>
2023-12-17 14:47:55 +0000

    leg angle rl

--- sketches/sketches.ino
+++ sketches/sketches.ino
@@ -1,38 +1,27 @@
 #include "/home/adaypa/Programming/RobotDog/src/funciones.cpp"
 #include "/home/adaypa/Programming/RobotDog/src/pin_config.h"
 
-int delay_ = 5;
-int speed = 20;
-
 void setup() {
 // ! ||--------------------------------------------------------------------------------||
 // ! ||                                    Servo attach                                ||
 // ! ||--------------------------------------------------------------------------------||
-  sRLArm.attach(rLArm);
-  sRLWrist.attach(rLWrist);
-  sRLShoulder.attach(rLShoulder);
+
   sRRArm.attach(rRArm);
   sRRWrist.attach(rRWrist);
   sRRShoulder.attach(rRShoulder);
 
-  sFLArm.attach(fLArm);
-  sFLWrist.attach(fLWrist);
-  sFLShoulder.attach(fLShoulder);
-  sFRArm.attach(fRArm);
-  sFRWrist.attach(fRWrist);
-  sFRShoulder.attach(fRShoulder);
+  sRLArm.attach(rLArm);
+  sRLWrist.attach(rLWrist);
+  sRLShoulder.attach(rLShoulder);
+
+  sRRShoulder.write(105);
+  sRRArm.write(125);
+  sRRWrist.write(40);
+
+  sRLShoulder.write(80);
+  sRLArm.write(81);
+  sRLWrist.write(116);
 }
 
 void loop() {
-  moveRL(sRLArm,sRLWrist,delay_,speed);
 }
-
-/*
-81.59978937660634        116.92766301978149
-73.338522423467        141.52513353107213
-86.06604337823569        160.42852932233336
-87.88613930259771        200
-35.50432720993205        190.36141597438956
-35.50432720993205        133.6223638011749
-56.648097576647665        112.15161442078205
-*/
